67c6cb51cddcc919ce2d3751343531b6257f92f1
Alessio Pancia <alessiop2801@gmail.com>
2020-12-17 15:06:57 +0100

    select dinamici

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var app = new Vue({
   el: "#root",
   data: {
-    cards: []
+    cards: [],
+    genres: [],
+    select: ""
   },
   mounted() {
     axios
@@ -9,5 +11,25 @@ var app = new Vue({
      .then((arr) => {
        arr.data.response.forEach((element) => (this.cards.push(element)))
      });
+
+    axios
+      .get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((arr) => {
+        arr.data.response.forEach((element) => {
+          var notPush = false;
+          this.genres.forEach((genre, index) => {
+            if (element.genre == genre) {
+              notPush = true;
+            }
+          });
+
+          if(notPush == false) {
+            this.genres.push(element.genre);
+          }
+        })
+      });
+  },
+  methods: {
+  
   }
 })
